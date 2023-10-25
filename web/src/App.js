32b0384c58fbd158b87eb093960f9a6e8bb19a99
Jonas <jonashenrique989@gmail.com>
2023-10-25 18:18:30 -0300

    add blocos iniciais initialNodes

--- web/src/App.js
+++ web/src/App.js
@@ -1,31 +1,150 @@
 
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import ReactFlow, {
 	Background,
 	Controls,
+	applyEdgeChanges,
+	applyNodeChanges,
+	addEdge,
 	MiniMap,
 	SelectionMode,
 } from 'reactflow';
+
 import { Master } from './components/Master';
+import { Building } from './components/Building';
+import { Floor } from './components/Floor';
+import { Room } from './components/Room';
 
 
 const initialNodes = [
 	{
-		id: '2378230',
+		id: 'master-1',
 		position: { x: 0, y: 0 },
 		data: { label: 'UFJF Central' },
 		type: 'Master',
         draggable: true,
-	}
+	},
+
+	{
+		id: 'Building-1',
+		position: { x: -400, y: 120 },
+		data: { label: 'Prédio ICE' },
+		type: 'Building',
+        draggable: true,
+	},
+	{
+		id: 'Building-2',
+		position: { x: 400, y: 120 },
+		data: { label: 'Prédio Computação' },
+		type: 'Building',
+        draggable: true,
+	},
+
+	{
+		id: 'Floor-1',
+		position: { x: -600, y: 240 },
+		data: { label: 'Primeiro andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+	{
+		id: 'Floor-2',
+		position: { x: -400, y: 240 },
+		data: { label: 'Segundo andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+	{
+		id: 'Floor-3',
+		position: { x: -200, y: 240 },
+		data: { label: 'Terceiro andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+	{
+		id: 'Floor-4-',
+		position: { x: -200, y: 240 },
+		data: { label: 'Quarto andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+	{
+		id: 'Floor-4',
+		position: { x: 200, y: 240 },
+		data: { label: 'Primeiro andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+	{
+		id: 'Floor-5',
+		position: { x: 400, y: 240 },
+		data: { label: 'Segundo andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+	{
+		id: 'Floor-6',
+		position: { x: 600, y: 240 },
+		data: { label: 'Terceiro andar' },
+		type: 'Floor',
+        draggable: true,
+	},
+
+	{
+		id: 'room-1',
+		position: { x: -600, y: 400 },
+		data: { label: 'Sala S401', temp: 25, status: 'ok' },
+		type: 'Room',
+        draggable: true,
+	},
+	{
+		id: 'room-2',
+		position: { x: -600, y: 550 },
+		data: { label: 'Sala S402', temp: 25, status: 'ok' },
+		type: 'Room',
+        draggable: true,
+	},
+	{
+		id: 'room-3',
+		position: { x: -600, y: 700 },
+		data: { label: 'Sala S403', temp: 25, status: 'ok' },
+		type: 'Room',
+        draggable: true,
+	},
+	{
+		id: 'room-4',
+		position: { x: -600, y: 850 },
+		data: { label: 'Sala S404', temp: 70, status: 'Incêndio' },
+		type: 'Room',
+        draggable: true,
+	},
 ];
 
 const initialEdges = [
+	{ id: 'e-2', source: 'master-1', target: 'Building-1' },
+	{ id: 'e1-2', source: 'master-1', target: 'Building-2' },
+	
+	{ id: 'e1-2', source: 'Building-1', target: 'Floor-1' },
+	{ id: 'e1-2', source: 'Building-1', target: 'Floor-2' },
+	{ id: 'e1-2', source: 'Building-1', target: 'Floor-3' },
+		
+	{ id: 'e1-2', source: 'Building-2', target: 'Floor-4' },
+	{ id: 'e1-2', source: 'Building-2', target: 'Floor-5' },
+	{ id: 'e1-2', source: 'Building-2', target: 'Floor-6' },
+	
+
+	{ id: 'e1-2', source: 'Floor-1', target: 'room-1' },
+	{ id: 'e1-2', source: 'Floor-1', target: 'room-2' },
+	{ id: 'e1-2', source: 'Floor-1', target: 'room-3' },
+	{ id: 'e1-2', source: 'Floor-1', target: 'room-4' },
 ];
 
 const NODE_TYPES = {
-	master: Master,
-
+	Master: Master,
+	Building: Building,
+	Floor: Floor,
+	Room: Room,
 }
 const panOnDrag = [1, 2];
 
@@ -35,13 +154,21 @@ export default function App() {
 	const [edges, setEdges] = useState(initialEdges);
 
 
+	const onNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), []);
+	const onEdgesChange = useCallback((changes) => setEdges((eds) => applyEdgeChanges(changes, eds)), []);
+
+	const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
 	return (
 		<div style={{ width: '100vw', height: '100vh' }}>
 			
 			<ReactFlow
 				nodeTypes={NODE_TYPES}
 				nodes={nodes}
+				onNodesChange={onNodesChange}
 				edges={edges}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
 				
 				panOnScroll
 				selectionOnDrag={true}
