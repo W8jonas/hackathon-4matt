bdcc180a91a6d53e985d80a96a516cdf904c588c
Jonas <jonashenrique989@gmail.com>
2023-10-26 18:33:04 -0300

    Instalando MUI

--- web/src/index.js
+++ web/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ReactFlowProvider } from 'reactflow';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css'
 import 'reactflow/dist/style.css';
-import { ReactFlowProvider } from 'reactflow';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
