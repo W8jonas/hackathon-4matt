cfd551210c7736268859ed4b3c250399da5dd82c
Jonas <jonashenrique989@gmail.com>
2023-10-25 21:46:59 -0300

    Refatorando para leitura dos dados pelo Controller

--- web/src/App.js
+++ web/src/App.js
@@ -15,140 +15,8 @@ import { Master } from './components/Master';
 import { Building } from './components/Building';
 import { Floor } from './components/Floor';
 import { Room } from './components/Room';
+import { Data } from './Controllers/Data';
 
-
-const Buildings = [
-	{
-		id: 'Building-1',
-		position: { x: -400, y: 120 },
-		data: { label: 'Prédio ICE' },
-		type: 'Building',
-        draggable: true,
-	},
-	{
-		id: 'Building-2',
-		position: { x: 400, y: 120 },
-		data: { label: 'Prédio Computação' },
-		type: 'Building',
-        draggable: true,
-	},
-]
-
-const floors = [
-	{
-		id: 'Floor-1',
-		position: { x: -600, y: 240 },
-		data: { label: 'Primeiro andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-	{
-		id: 'Floor-2',
-		position: { x: -400, y: 240 },
-		data: { label: 'Segundo andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-	{
-		id: 'Floor-3',
-		position: { x: -200, y: 240 },
-		data: { label: 'Terceiro andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-	{
-		id: 'Floor-4-',
-		position: { x: -200, y: 240 },
-		data: { label: 'Quarto andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-	{
-		id: 'Floor-4',
-		position: { x: 200, y: 240 },
-		data: { label: 'Primeiro andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-	{
-		id: 'Floor-5',
-		position: { x: 400, y: 240 },
-		data: { label: 'Segundo andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-	{
-		id: 'Floor-6',
-		position: { x: 600, y: 240 },
-		data: { label: 'Terceiro andar' },
-		type: 'Floor',
-        draggable: true,
-	},
-]
-
-const rooms = [
-	
-	{
-		id: 'room-1',
-		position: { x: -600, y: 400 },
-		data: { label: 'Sala S401', temp: 25, status: 'ok' },
-		type: 'Room',
-        draggable: true,
-	},
-	{
-		id: 'room-2',
-		position: { x: -600, y: 550 },
-		data: { label: 'Sala S402', temp: 25, status: 'ok' },
-		type: 'Room',
-        draggable: true,
-	},
-	{
-		id: 'room-3',
-		position: { x: -600, y: 700 },
-		data: { label: 'Sala S403', temp: 25, status: 'ok' },
-		type: 'Room',
-        draggable: true,
-	},
-	{
-		id: 'room-4',
-		position: { x: -600, y: 850 },
-		data: { label: 'Sala S404', temp: 70, status: 'Incêndio' },
-		type: 'Room',
-        draggable: true,
-	},
-]
-
-const allNodes = [
-	{
-		id: 'master-1',
-		position: { x: 0, y: 0 },
-		data: { label: 'UFJF Central' },
-		type: 'Master',
-        draggable: true,
-	},
-	...Buildings,
-	...floors,
-	...rooms,
-];
-
-const initialEdges = [
-	{ id: 'e-2', source: 'master-1', target: 'Building-1' },
-	{ id: 'e1-2', source: 'master-1', target: 'Building-2' },
-	
-	{ id: 'e1-2', source: 'Building-1', target: 'Floor-1' },
-	{ id: 'e1-2', source: 'Building-1', target: 'Floor-2' },
-	{ id: 'e1-2', source: 'Building-1', target: 'Floor-3' },
-		
-	{ id: 'e1-2', source: 'Building-2', target: 'Floor-4' },
-	{ id: 'e1-2', source: 'Building-2', target: 'Floor-5' },
-	{ id: 'e1-2', source: 'Building-2', target: 'Floor-6' },
-	
-
-	{ id: 'e1-2', source: 'Floor-1', target: 'room-1' },
-	{ id: 'e1-2', source: 'Floor-1', target: 'room-2' },
-	{ id: 'e1-2', source: 'Floor-1', target: 'room-3' },
-	{ id: 'e1-2', source: 'Floor-1', target: 'room-4' },
-];
 
 
 const NODE_TYPES = {
@@ -157,12 +25,22 @@ const NODE_TYPES = {
 	Floor: Floor,
 	Room: Room,
 }
-const panOnDrag = [1, 2];
+
+
+function mergeData(oldData, newData) {
+	const mergeSet = {}
+
+	oldData.forEach(data => mergeSet[data.id] = {...data})
+	newData.forEach(data => mergeSet[data.id] = {...data})
+
+	return Object.values(mergeSet)
+}
+
 
 export default function App() {
 	
-    const [nodes, setNodes] = useState(allNodes);
-	const [edges, setEdges] = useState(initialEdges);
+    const [nodes, setNodes] = useState([]);
+	const [edges, setEdges] = useState([]);
 
 	const onNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), []);
 	const onEdgesChange = useCallback((changes) => setEdges((eds) => applyEdgeChanges(changes, eds)), []);
@@ -171,8 +49,29 @@ export default function App() {
 
 
 	useEffect(() => {
-		
+		function onUpdate(ICs) {
+			setNodes(old => mergeData(old, ICs))
+		}
+
+		const unsubscribe = Data().getICs(onUpdate)
+		return () => unsubscribe
 	}, [])
+
+	useEffect(() => {
+		function onUpdate(services) {
+			setNodes(old => mergeData(old, services))
+		}
+
+		const unsubscribe = Data().getServices(onUpdate)
+		return () => unsubscribe
+	}, [])
+
+	useEffect(() => {
+		Data().getRelationships().then(
+			data => setEdges(data)
+		)
+	}, [])
+
 
 	return (
 		<div style={{ width: '100vw', height: '100vh' }}>
@@ -186,7 +85,7 @@ export default function App() {
 				
 				panOnScroll
 				selectionOnDrag={true}
-				panOnDrag={panOnDrag}
+				panOnDrag={[1, 2]}
 				selectionMode={SelectionMode.Partial}
 				selectNodesOnDrag={true}
 				
