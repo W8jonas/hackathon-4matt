6cce2db1d46662079aa6513effb571e86ab75f7c
Jonas <jonashenrique989@gmail.com>
2023-10-25 22:36:30 -0300

    add deleteDocuments

---
+++ functions/utils/deleteDocuments.js
@@ -0,0 +1,12 @@
+
+async function deleteDocuments(collectionName, docsIds) {
+	const promises = docsIds.map(
+		async (documentId) => admin.firestore().collection(collectionName).doc(documentId).delete(),
+	);
+
+	return Promise.all(promises);
+}
+
+module.exports = {
+	deleteDocuments,
+};
