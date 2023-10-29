b4dfeca83d8d7164964abb56f58e155bf121fec1
Jonas <jonashenrique989@gmail.com>
2023-10-29 20:57:05 -0300

    add header buttons para webhooks

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -3,21 +3,16 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 
 import logo from "../assets/Logo_da_UFJF_2.png"
 import './header.css';
+import { FormControlLabel, FormGroup, Switch } from '@mui/material';
 
 const pages = ['Painel', 'Alunos'];
 
-export function Header({onLayoutClick}) {
+export function Header({ onLayoutClick, serviceNowActive, onActiveServiceNow, webhookSiteActive, onActiveWebhookSite }) {
 
     return (
         <AppBar position="static" className="appHeader">
@@ -29,7 +24,7 @@ export function Header({onLayoutClick}) {
                         {pages.map((page) => (
                             <Button
                                 key={page}
-                                onClick={()=>{}}
+                                onClick={() => { }}
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                             >
                                 {page}
@@ -37,15 +32,41 @@ export function Header({onLayoutClick}) {
                         ))}
                     </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Button
-                            key={'layout-button'}
-                            onClick={onLayoutClick && onLayoutClick}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            Layout
-                        </Button>
-                    </Box>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={serviceNowActive}
+                                    onChange={onActiveServiceNow}
+                                    inputProps={{ 'aria-label': 'controlled' }}
+                                    size="small"
+                                    color="success"
+                                />
+                            }
+                            label="Ativar webhook serviceNow"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={webhookSiteActive}
+                                    onChange={onActiveWebhookSite}
+                                    inputProps={{ 'aria-label': 'controlled' }}
+                                    size="small"
+                                    color="success"
+                                />
+                            }
+                            label="Ativar webhook site"
+                        />
+                    </FormGroup>
+
+                    <Button
+                        key={'layout-button'}
+                        id={'layout-button'}
+                        onClick={onLayoutClick && onLayoutClick}
+                        sx={{ display: 'none' }}
+                    >
+                        Layout
+                    </Button>
                 </Toolbar>
             </Container>
         </AppBar>
