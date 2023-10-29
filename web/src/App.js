c816011cb5a8a8becfa259dc72267ebf2126eb51
Jonas <jonashenrique989@gmail.com>
2023-10-29 19:48:50 -0300

    criando função para getDeepLocation

--- web/src/App.js
+++ web/src/App.js
@@ -56,6 +56,25 @@ function mergeData(oldData, newData) {
 
 // setRelationshipDB()
 
+function getDeepLocation(rawLocation) {
+
+	function getLocation(actualLocation, _location) {
+		if (_location.includes(':')) {
+			return {
+				...actualLocation,
+				entityName: _location.split(':')[0],
+				children: getLocation({}, _location.substr(Array.from(_location).findIndex(_letter => _letter === ':') + 1))
+			}
+        }
+
+		return {
+			entityName: _location.split(':')[0],
+			children: null
+		}
+	}
+	return getLocation({}, rawLocation)
+}
+
 export default function App() {
 	const { fitView } = useReactFlow();
 
