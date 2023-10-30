78f2b2066a0b92b6a5d27ff398240e7b3d7c3533
Jonas <jonashenrique989@gmail.com>
2023-10-30 00:53:39 -0300

    Add webhookServiceNow + node-fetch

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ admin.initializeApp();
 const { readDocuments } = require("./utils/readDocuments");
 const { deleteDocuments } = require("./utils/deleteDocuments");
 const { updateDocument } = require("./utils/updateDocument");
+const fetch = require("node-fetch");
 
 
 exports.resetRooms = onRequest({
@@ -17,6 +18,10 @@ exports.resetRooms = onRequest({
         const data = await readDocuments('services')
         await deleteDocuments('services', data.map(doc => doc.id))
 
+        response.set({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        })
         response.status(200).json("Operação concluida!");
     } catch (error) {
         console.log('error > ', error)
@@ -26,7 +31,6 @@ exports.resetRooms = onRequest({
 
 exports.updateRoom = onRequest({
     cors: ['*'],
-    
 }, async (request, response) => {
     try {
 	    const {id: roomId} = request.query;
@@ -34,7 +38,40 @@ exports.updateRoom = onRequest({
         
         const resp = await updateDocument('services', roomId, data)
 
+        response.set({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        })
         response.status(200).json(resp)
+    } catch (error) {
+        console.log('error > ', error)
+        response.status(500).json("error!", error?.message);
+    }
+});
+
+
+exports.webhookServiceNow = onRequest({
+    cors: ['*', 'hackathon-4matt.web.app'],
+}, async (request, response) => {
+    try {
+        const data = request.body
+        
+        const resp = await fetch('https://4matttecnologiadainformacaoltdademo3.service-now.com/api/now/table/incident', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa('ufjf.hackathon:@Ufjf2023'),
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(JSON.parse(data))
+        })
+        const respJson = await resp.json()
+
+        response.set({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        })
+        response.json(respJson)
     } catch (error) {
         console.log('error > ', error)
         response.status(500).json("error!", error?.message);
