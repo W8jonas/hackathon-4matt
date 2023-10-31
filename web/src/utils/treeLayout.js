53e85e5621adb087a982aa101c6e56e93f26fa3a
Jonas <jonashenrique989@gmail.com>
2023-10-31 14:49:32 -0300

    reorganizando arvore com base na largura da tela

--- web/src/utils/treeLayout.js
+++ web/src/utils/treeLayout.js
@@ -7,23 +7,28 @@ const g = tree();
 export function getLayoutedElements(nodes, edges, options) {
 	if (nodes.length === 0) return { nodes, edges };
 
-	const { width, height } = document
-		.querySelector(`[data-id="${nodes[0].id}"]`)
-		.getBoundingClientRect();
+	const {innerWidth, innerHeight} = window
+
+	const maxWidth = Math.min(innerWidth, 1400)
+	const maxHeight = Math.min(innerHeight, 1250)
+
+	// const { width, height } = document
+	// 	.querySelector(`[data-id="${nodes[0].id}"]`)
+	// 	.getBoundingClientRect();
 
 	const hierarchy = stratify()
 		.id((node) => node.id)
 		.parentId((node) => edges.find((edge) => edge.target === node.id)?.source);
 
 	const root = hierarchy(nodes);
-	const layout = g.nodeSize([width / 3.2, height * 1.5])(root);
+	const layout = g.nodeSize([maxWidth / 12, maxHeight / 5])(root);
 
 	return {
 		nodes: layout
 			.descendants()
 			.map((node) => {
                 if (node.depth === 3) {
-                    return { ...node.data, position: { x: node.parent.x, y: node?.parent?.y + (250 * (node.id[19]) - 1) - 50 } }
+                    return { ...node.data, position: { x: node.parent.x, y: node?.parent?.y + (270 * (node.id[19]) - 1) - 50 } }
                 }
                 return { ...node.data, position: { x: node.x, y: node.y } }
             }),
